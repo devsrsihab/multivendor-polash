f6cf90db3b510d5e9ac3cc412c0aa376a4ac6bee
devsrsihab <91025640+devsrsihab@users.noreply.github.com>
2023-05-16 11:33:12 +0600

    Multivendor Polash

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\CPU\Helpers;
+use App\Model\Admin;
+use App\CPU\ImageManager;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
+use Brian2694\Toastr\Facades\Toastr;
+use Illuminate\Support\Facades\File;
+
+class ProfileController extends Controller
+{
+
+    public function view()
+    {
+        $data = Admin::where('id', auth('admin')->id())->first();
+        return view('admin-views.profile.view', compact('data'));
+    }
+
+    public function edit($id)
+    {
+        $data = Admin::where('id', $id)->first();
+        $shop_banner = Helpers::get_business_settings('shop_banner');
+        return view('admin-views.profile.edit', compact('data', 'shop_banner'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $admin = Admin::find($id);
+        $admin->name = $request->name;
+        $admin->phone = $request->phone;
+        $admin->email = $request->email;
+        // if ($request->image) {
+        //     $admin->image = ImageManager::update('admin/', $admin->image, 'png', $request->file('image'));
+        // }
+
+
+        if ($request->hasfile('image')) {
+            $path = 'assets/app/profile'. $admin->photo;
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+
+            $file = $request->file('image');
+            $extenttion = $file->getClientOriginalExtension();
+  
+            $fileName = Carbon::now()->toDateString() . "-" . uniqid() . "." . $extenttion;
+            $file->move('assets/app/profile', $fileName);
+
+            // photo save in database
+            $admin->image = $fileName;
+        }
+
+
+        $admin->save();
+        Toastr::info('Profile updated successfully!');
+        return back();
+    }
+
+    public function settings_password_update(Request $request)
+    {
+        $request->validate([
+            'password' => 'required|same:confirm_password|min:8',
+            'confirm_password' => 'required',
+        ]);
+
+        $admin = Admin::find(auth('admin')->id());
+        $admin->password = bcrypt($request['password']);
+        $admin->save();
+        Toastr::success('Admin password updated successfully!');
+        return back();
+    }
+
+}
